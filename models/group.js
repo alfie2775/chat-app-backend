587e846a8ae7f663b2859c22229bf4d5bb17860d
alfie2775 <nunetarun2001@gmail.com>
2021-05-24 14:17:47 +0530

    fixed: timestamps for groups

--- models/group.js
+++ models/group.js
@@ -1,28 +1,33 @@
 const mongoose = require("mongoose");
 const { groupMessageSchema } = require("./groupMessage");
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  members: [
-    {
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    members: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [groupMessageSchema],
+    admins: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    createdBy: {
       type: mongoose.Types.ObjectId,
       ref: "User",
     },
-  ],
-  messages: [groupMessageSchema],
-  admins: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Group", schema);
